3316fb97cb1894048a311609e418e2852e49e231
Nurken <nurkenqaldybaev2001@gmail.com>
2025-03-25 16:40:00 +0500

    configs added and app.js setup

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import app from "./app.js";
+import connectDB from "./config/mongo.config.js"
+import {PORT} from "./config/app.config.js"
+
+await connectDB()
 
 
-app.listen(3000, () => {
-    console.log(`Server in running on port ${3000}`)
+app.listen(PORT, () => {
+    console.log(`Server in running on port ${PORT}`)
 })
