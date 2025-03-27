1861cd5cc6dbdfb9f55388fd25dab703f85b0b32
Nurken <nurkenqaldybaev2001@gmail.com>
2025-03-27 17:22:09 +0500

    food model added

--- src/controller/food.controller.js
+++ src/controller/food.controller.js
@@ -1,25 +1,128 @@
+import { isValidObjectId } from "mongoose"
 import foodModel from "../model/food.model.js"
+import categoryModel from "../model/category.model.js";
+
 
 const getAllFoods = async (req, res) => {
-    const products = await foodModel.find().populate("category")
+    const products = await foodModel.find()
+        .populate({
+            path: "category",
+            select: "-foods -createdAt -updatedAt"
+        })
+        .select("-createdAt -updatedAt");
 
     res.send({
         message: "Success✅",
         count: products.length,
         data: products
-    })
+    });
+}
+
+const getOneFood = async (req, res) => {
+    const { id } = req.params;
+
+    if (!isValidObjectId(id)) {
+        return res.status(400).send({
+            message: `Given ID: ${id} is not valid Object ID`
+        });
+    }
+
+    const food = await foodModel.findById(id)
+        .populate({
+            path: "category",
+            select: "-foods -createdAt -updatedAt"
+        })
+        .select("-createdAt -updatedAt");
+
+    if (!food) {
+        return res.status(404).send({ message: "Taom topilmadi ❌" });
+    }
+
+    res.send({
+        message: "Success✅",
+        data: food
+    });
 }
 
 const createFood = async (req, res) => {
-    const { name, description, price, category, imageUrl } = req.body
-
+    const { name, price, category, description, imageUrl } = req.body;
+  
+    const foundedCategory = await categoryModel.findById(category);
+  
+    if (!foundedCategory) {
+      return res.status(404).send({
+        message: `Category with ID: ${category} not found`,
+      });
+    }
+  
     const food = await foodModel.create({
-        name, description, price, category, imageUrl
-    })
-
+      name,
+      price,
+      category,
+      description,
+      imageUrl,
+    });
+  
+    await categoryModel.updateOne(
+      { _id: category },
+      {
+        $push: {
+          foods: food._id,
+        },
+      }
+    );
+  
     res.status(201).send({
-        message: "Success",
+      message: "success",
+      data: food,
+    });
+  };
+  
+
+const updateFood = async (req, res) => {
+    const { id } = req.params;
+
+    if (!isValidObjectId(id)) {
+        return res.status(400).send({
+            message: `Given ID: ${id} is not valid Object ID`
+        });
+    }
+
+    const { name, price, description } = req.body;
+
+    const food = await foodModel.findByIdAndUpdate(
+        id,
+        { name, description, price },
+        { new: true }
+    );
+
+    if (!food) {
+        return res.status(404).send({ message: "Taom topilmadi ❌" });
+    }
+
+    res.send({
+        message: "Yangilandi ✅",
         data: food
-    })
+    });
 }
-export default { getAllFoods,createFood }
+
+const deleteFood = async (req, res) => {
+    const { id } = req.params;
+
+    if (!isValidObjectId(id)) {
+        return res.status(400).send({
+            message: `Given ID: ${id} is not valid Object ID`
+        });
+    }
+
+    const food = await foodModel.findById(id);
+    if (!food) {
+        return res.status(404).send({ message: "Taom topilmadi ❌" });
+    }
+
+    await foodModel.deleteOne({ _id: id });
+
+    res.status(204).send();
+}
+
+export default { getAllFoods, getOneFood, createFood, deleteFood, updateFood };
--- src/routes/food.route.js
+++ src/routes/food.route.js
@@ -6,5 +6,8 @@ const foodRouter = Router()
 foodRouter
     .get("/", foodController.getAllFoods)
     .post("/", foodController.createFood)
+    .get("/:id",foodController.getOneFood)
+    .patch("/:id",foodController.updateFood)
+    .delete("/:id",foodController.deleteFood)
 
 export default foodRouter
